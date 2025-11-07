3e937fede7f81ffd581624b5f8ae977100a34e07
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-11-07 14:22:11 +0100

    Add folder Models in Taskboard.Core | add TaskItem and User classes

---
+++ Taskboard.Core/Models/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskboard.Core.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string? Description { get; set; }
+        //Status can be "Open", "In Progress", "Completed"
+        public string Status { get; set; } = "Open";
+        //Priority can be "Low", "Medium", "High"
+        public string Priority { get; set; } = "Medium"; 
+        public DateTime? DueDate { get; set; }
+        public int? AssignedUserId { get; set; }
+        public User? AssignedUser { get; set; }
+    }
+}
